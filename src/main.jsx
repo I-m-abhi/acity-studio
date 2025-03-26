732953808ae69f2f217fb56743fe7b8f4b37797c
I=m-abhi <abhisasa2001@gmail.com>
2025-03-26 05:43:48 +0530

    npm i react-gtm-module

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,13 @@ import './styles/app.css'
 import './styles/style.css'
 import './styles/responsive.css'
 import App from './App.jsx'
+import TagManager from "react-gtm-module";
+
+const tagManagerArgs = {
+  gtmId: "AW-16520452127",
+};
+
+TagManager.initialize(tagManagerArgs);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
